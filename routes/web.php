44d98904654c80560c409c391593ae5fc9e9c1c5
victra <victra.sonata@gmail.com>
2017-04-04 13:50:49 +0700

    entahlah

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(array('prefix' => '/'), function() {
+  Route::get('', function () {return view('welcome');});
+  Route::get('theme', 'Theme\ThemeController@index');
+});
